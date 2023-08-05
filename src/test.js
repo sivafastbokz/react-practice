988ce7c923fe19ae60447bbb496b5b9a48846eed
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-08-05 19:23:34 +0530

    practiced

--- src/test.js
+++ src/test.js
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-
-
 function Test(){
     const[data,setdata]=useState('posts');
     const[item,setItem]=useState([]);
+    console.log('useState')
 
     useEffect(()=>{
         fetch(`https://jsonplaceholder.typicode.com/${data}`)
           .then(response => response.json())
           .then(json => setItem(json))
-        console.log('data')
+        console.log('useEffect:data')
         return()=>{
-            console.log('data has changed')
+            console.log('useEffect:data has changed')
         }
     },[data])
     return(
      <React.Fragment>
+        {console.log('div')}
       <button onClick={()=>setdata('posts')}>post</button>
       <button onClick={()=>setdata('comments')}>comments</button>
       <button onClick={()=>setdata('users')}>users</button>
